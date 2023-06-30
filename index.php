e61e43376c7619a72599a06fbdbffe69f4b8d75e
Marynamore <marinaoliverbri674@gmail.com>
2023-06-30 11:10:57 -0300

    Atualizado 30/06

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ if (isset($_SESSION['msg'])) {
     <script type="text/javascript" src="js/modal.js"></script>
     <link href="css/modal.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Meu Projeto</title>
 </head>
 <body>
@@ -33,84 +34,105 @@ if (isset($_SESSION['msg'])) {
     <header class="main_header">
         <div class="main_header_content">
             <a href="#" class="logo">
-                <img src="img/logo.png" alt="Bem vindo ao projeto prático Html5 e Css3 Essentials" title="Bem vindo ao projeto prático Html5 e Css3 Essentials">
+                <img src="img/logo.png" alt="Bem vindo ao projeto usuário">
             </a>
             <nav class="main_header_content_menu">
                 <ul>
-                    <li><a href="">Home</a></li>
+                    <li><a href=""><i class="fa-solid fa-house"></i>HOME</a></li>
                     <?php
                         if (!empty($usuarioLogado)) {
                             if ($id_perfil == 1) {
-                                echo '<a href="./view/dashboard/painel_adm.php"><i class="fa-solid fa-user"></i>Painel Administrador</a>';
-                                echo '<a class="border1" href="./control/control_sair.php" class="item_menu"><i class="fa-solid fa-right-from-bracket"></i>SAIR</a>';
+                                echo '<li><a href="./view/dashboard/painel_adm.php"><i class="fa-solid fa-user"></i>Painel Administrador</a></li>';
+                                echo '<li><a class="border1" href="./control/control_sair.php" class="item_menu"><i class="fa-solid fa-right-from-bracket"></i>SAIR</a></li>';
                             } elseif ($id_perfil == 2) {
-                                echo '<a href="./view/dashboard/painel_moderador.php"><i class="fa-solid fa-users"></i> PAINEL MODERADOR</a>';
-                                echo '<a class="border1" href="./control/control_sair.php" class="item_menu"><i class="fa-solid fa-right-from-bracket"></i>SAIR</a>';
+                                echo '<li><a href="./view/dashboard/painel_moderador.php"><i class="fa-solid fa-users"></i> PAINEL MODERADOR</a></li>';
+                                echo '<li><a class="border1" href="./control/control_sair.php" class="item_menu"><i class="fa-solid fa-right-from-bracket"></i>SAIR</a></li>';
                             } elseif ($id_perfil == 3 || $id_perfil == 4) {
-                                echo '<a href="./view/perfil_usuario.php?id_usuario=' . $id_usuarioLogado . '" onclick="funcPerfil()"><i class="fa-solid fa-user"></i>' . $usuarioLogado . '</a>';
-                                echo '<a class="border1" href="./control/control_sair.php" class="item_menu"><i class="fa-solid fa-right-from-bracket"></i>SAIR</a>';
+                                echo '<li><a href="./view/perfil_usuario.php?id_usuario=' . $id_usuarioLogado . '" onclick="funcPerfil()"><i class="fa-solid fa-user"></i>' . $usuarioLogado . '</a></li>';
+                                echo '<li><a class="border1" href="./control/control_sair.php" class="item_menu"><i class="fa-solid fa-right-from-bracket"></i>SAIR</a></li>';
                             }
                         } else {
-                            echo '<a href="./view/cadastro_usuario.php"><i class="fa-solid fa-user"></i>CADASTRO</a>';
-                            echo '<a href="" class="modal-link"><i class="fa-solid fa-user"></i>LOGIN</a>';
-                            echo '<a href="#contato">Contato</a>';
+                            echo '<li><a href="./view/cadastro_usuario.php"><i class="fa-solid fa-user"></i>CADASTRO</a></li>';
+                            echo '<li><a href="" class="modal-link"><i class="fa-solid fa-user"></i>LOGIN</a></li>';
+                            echo '<li><a href="#contato">CONTATO</a></li>';
                         }
                     ?>
                 </ul>
             </nav>
         </div>
     </header>
-
-    <!--POP LOGIN-->
-        <div class="overlay"></div>
+<!--POP LOGIN-->
+    <div class="overlay"></div>
         <div class="modal">
-            
-                <div class="div_login">
-                    <form action="control/login_control.php" method="POST">
-                    <h1>Login</h1><br>
-                    <input type="text" placeholder="Email" name="email" class="input">
-                    <br><br>
-                    <input type="password" placeholder="Senha" name="senha" class="input">
-                    <br><br>
-                    <button class="button">Enviar</button>
-                </form>
-                </div>
-            
+            <div class="div_login">
+                <form action="control/login_control.php" method="POST">
+                <h1>Login</h1><br>
+                <input type="text" placeholder="Email" name="email" class="input">
+                <br><br>
+                <input type="password" placeholder="Senha" name="senha" class="input">
+                <br><br>
+                <button class="button">Enviar</button>
+            </form>
         </div>
-    <!--FIM POP LOGIN-->
-
+        
+    </div>
+<!--FIM POP LOGIN-->
     <main>
-        <h2>Bem-vindo ao Meu Projeto</h2>
-        <p>Esta é a página inicial do projeto. Aqui você pode adicionar seu conteúdo principal.</p>
-    </main>
-
-    <footer>
-        <section class="main_optin_footer">
-            <div class="main_optin_footer_content" >
-                <header>
-                    <h1>Quer receber nosso conteúdo exclusivo? Assina nossa lista VIP :)</h1>
-                </header>
-                <article>
+        <section class="main_cta">
+            <article class="main_cta_content">
+                <div class="main_cta_content_spacer">
                     <header>
-                        <h2><a href="#" class="btn">Entrar para a lista VIP</a></h2>
+                        <h1>Bem-vindo ao Meu Projeto</h1>
                     </header>
-                </article>
-            </div>
+                    <p>Aqui você encontrará recursos exclusivos para clientes, administradores e moderadores.</p>
+                </div>
+            </article>
         </section>
-
+        <section class="main_blog" id="seuespaco">
+            <header class="main_blog_header">
+                <h1>Seu espaço</h1>
+            </header>
+            <aside>
+                <div class="imagens_acesso">
+                    <img src="assets/img/cliente.png" alt="Imagem post" title="Cliente" class="imagem1">
+                    <p class="category">Área do Cliente</p><br>
+                    <p>Acesse aqui os recursos exclusivos disponíveis para clientes.</p>
+                    <br>
+                    <p><a href="telascliente.php" class="btn">Acesse</a></p>
+                </div>
+            </aside>
+            <aside>
+                <div class="imagens_acesso">
+                    <img src="assets/img/moderador.png" alt="Imagem post" title="Moderador" class="imagem1">
+                    <p class="category">Área do Moderador</p><br>
+                    <p>Acesse aqui os recursos exclusivos disponíveis para moderadores.</p>
+                    <br>
+                    <p><a href="telamoderador.php" class="btn">Acesse</a></p>
+                </div>
+            </aside>
+            <aside>
+                <div class="imagens_acesso">
+                    <img src="assets/img/administrador.png" alt="Imagem post" title="Administrador" class="imagem1">
+                    <p class="category">Área do Administrador</p><br>
+                    <p>Acesse aqui os recursos exclusivos disponíveis para administradores.</p>
+                    <br>
+                    <p><a href="telaadministrador.php" class="btn">Acesse</a></p>
+                </div>
+            </aside>
+        </section>
+    </main>
+    <hr>
+    <footer>
         <section class="main_footer">
             <header>
                 <h1>Quer saber mais?</h1>
             </header>
             <article class="main_footer_our_pages">
                 <header>
-                    <h2>Nossas Páginas</h2>
+                    <h2>Meu Cartão Postal</h2>
                 </header>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">A Escola</a></li>
-                    <li><a href="#">Contato</a></li>
+                    <li><a href="https://marynamore.github.io/profile/">Minha Landing Page</a></li>
                 </ul>
             </article>
 
@@ -129,32 +151,12 @@ if (isset($_SESSION['msg'])) {
                 <header>
                     <h2>Sobre o Projeto</h2>
                 </header>
-                <p>Aprenda a trabalhar com HTML5 e CSS3 para desenvolver seus projetos e entregar páginas que estejam dentro
-                    dos padrões da WEB seguindo as boas práticas</p>
+                <p>O objetivo é permitir que os usuários realizem operações de criação, leitura, atualização e exclusão de informações em um banco de dados, fornecendo diferentes funcionalidades e permissões de acordo com o tipo de usuário logado.</p>
             </article>
         </section>
         <p>&copy; 2023 Meu Projeto. Todos os direitos reservados.</p>
     </footer>
-    <script>
-        // Seleciona o link e a janela modal
-        var link = document.querySelector('.modal-link');
-        var modal = document.querySelector('.modal');
-        var overlay = document.querySelector('.overlay');
-
-        // Adiciona um listener de evento para o link
-        link.addEventListener('click', function (event) {
-            event.preventDefault(); // previne o comportamento padrão do link (navegar para outra página)
-
-            overlay.style.display = 'block'; // exibe a camada escura
-            modal.style.display = 'block'; // exibe a janela modal
-        });
-
-        // Adiciona um listener de evento para a camada escura
-        overlay.addEventListener('click', function () {
-            overlay.style.display = 'none'; // oculta a camada escura
-            modal.style.display = 'none'; // oculta a janela modal
-        });
-    </script>
+    <script src="./js/script.js"></script>
 </body>
 </html>
 
--- sql/projeto_usu.sql
+++ sql/projeto_usu.sql
@@ -7,7 +7,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-
 -- -----------------------------------------------------
 -- Schema projeto_usu
 -- -----------------------------------------------------
@@ -19,14 +18,15 @@ DROP SCHEMA IF EXISTS `projeto_usu` ;
 CREATE SCHEMA IF NOT EXISTS `projeto_usu` DEFAULT CHARACTER SET utf8 ;
 USE `projeto_usu` ;
 
---
--- Estrutura da tabela `perfil`
---
+-- -----------------------------------------------------
+-- Table `projeto_usu`.`perfil`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `projeto_usu`.`perfil` ;
 
-CREATE TABLE `perfil` (
-  `id_perfil` int(11) NOT NULL,
-  `nome_perfil` varchar(45) NOT NULL,
-    PRIMARY KEY (`id_perfil`))
+CREATE TABLE IF NOT EXISTS `projeto_usu`.`perfil` (
+  `id_perfil` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_perfil` varchar(45) NOT NULL COMMENT 'Administrador\\nCliente\\nModerador',
+  PRIMARY KEY (`id_perfil`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -39,37 +39,35 @@ INSERT INTO `perfil` (`id_perfil`, `nome_perfil`) VALUES
 (2, 'Cliente'),
 (3, 'Moderador');
 
--- --------------------------------------------------------
-
 -- -----------------------------------------------------
 -- Table `projeto_usu`.`usuario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `cine_community`.`usuario` ;
+DROP TABLE IF EXISTS `projeto_usu`.`usuario` ;
 
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
-  `nome_usu` varchar(100) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `senha` varchar(50) DEFAULT NULL,
-  `telefone` varchar(20) DEFAULT NULL,
-  `cpf` varchar(14) DEFAULT NULL,
-  `sexo` varchar(12) DEFAULT NULL,
-  `dt_nascimento` date DEFAULT NULL,
-  `endereco` varchar(70) DEFAULT NULL,
-  `numero` char(7) DEFAULT NULL,
-  `complemento` varchar(15) DEFAULT NULL,
-  `bairro` varchar(70) DEFAULT NULL,
-  `cidade` varchar(70) DEFAULT NULL,
-  `cep` char(9) DEFAULT NULL,
-  `uf` char(2) DEFAULT NULL,
-  `foto` varchar(50) DEFAULT NULL,
-  `situacao` varchar(10) DEFAULT 'Ativo' COMMENT 'Ativo\\nCancelado\\nBloqueado',
-  `fk_id_perfil` int(11) NOT NULL,
+  `id_usuario` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome_usu` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(50) NOT NULL,
+  `telefone` VARCHAR(20) DEFAULT NULL,
+  `cpf` VARCHAR(14) DEFAULT NULL,
+  `sexo` ENUM('masculino', 'feminino', 'naoBinario', 'naoDeclarar') NOT NULL COMMENT 'Masculino\\nFeminino\\nNão Binário\\nNão Declarar',
+  `dt_nascimento` DATE DEFAULT NULL,
+  `endereco` VARCHAR(70) DEFAULT NULL,
+  `numero` CHAR(7) DEFAULT NULL,
+  `complemento` VARCHAR(15) DEFAULT NULL,
+  `bairro` VARCHAR(70) DEFAULT NULL,
+  `cidade` VARCHAR(70) DEFAULT NULL,
+  `cep` CHAR(9) DEFAULT NULL,
+  `uf` CHAR(2) DEFAULT NULL,
+  `foto` VARCHAR(50) DEFAULT NULL,
+  `situacao` VARCHAR(10) DEFAULT 'Ativo' COMMENT 'Ativo\\nCancelado\\nBloqueado',
+  `fk_id_perfil` INT(11) NOT NULL,
   PRIMARY KEY (`id_usuario`, `fk_id_perfil`),
   INDEX `fk_usuario_perfil_usu1_idx` (`fk_id_perfil` ASC) ,
   CONSTRAINT `fk_usuario_perfil_usu1`
     FOREIGN KEY (`fk_id_perfil`)
-    REFERENCES `cine_community`.`perfil` (`id_perfil`)
+    REFERENCES `projeto_usu`.`perfil` (`id_perfil`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION) 
 ENGINE = InnoDB
@@ -80,9 +78,9 @@ DEFAULT CHARACTER SET = utf8;
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nome_usu`, `email`, `senha`, `telefone`, `cpf`, `sexo`, `dt_nascimento`, `endereco`, `numero`, `complemento`, `bairro`, `cidade`, `cep`, `uf`, `foto`, `situacao`, `fk_id_perfil`) VALUES
-(1, 'Administrador', 'adm@email.com', md5('123'), '(61)77777-6565', '000.000.888-99', 'Masculino', '1989-03-10', 'QNM 11', '11', 'Casa', 'CEILANDIA NORTE', 'BrasÃ­lia', '72211-111', 'DF', NULL, 'Ativo', 1),
-(2, 'JosÃ© do Teste', 'jose@email.com', md5('123456'), '6198887-0909', '333.444.777-77', 'Feminino', '2000-02-01', 'QSC 01 AREA ESP ', '444', 'Ap', 'CEILANDIA NORTE', 'BRASILIA', '72-999-02', 'MG', NULL, 'Ativo', 2),
-(2, 'JosÃ© do Teste', 'jose@email.com', md5('123456'), '6198887-0909', '333.444.777-77', 'Feminino', '2000-02-01', 'QSC 01 AREA ESP ', '444', 'Ap', 'CEILANDIA NORTE', 'BRASILIA', '72-999-02', 'MG', NULL, 'Ativo', 3);
+(1, 'Administrador', 'adm@email.com', MD5('123'), '(61)77777-6565', '000.000.888-99', 'Masculino', '1989-03-10', 'QNM 11', '11', 'Casa', 'CEILÂNDIA NORTE', 'BRASÍLIA', '72211-111', 'DF', NULL, 'Ativo', 1),
+(2, 'João do Teste', 'jose@email.com', MD5('123456'), '6198887-0909', '333.444.777-77', 'Feminino', '2000-02-01', 'QSC 01 AREA ESP ', '444', 'Ap', 'CEILÂNDIA NORTE', 'BRASILIA', '72-999-02', 'DF', NULL, 'Ativo', 2),
+(3, 'Maya', 'maya@email.com', MD5('123456789'),'6198887-0909', '079.321.888-30', 'naoBinario', '2000-12-08', 'QSC 01 AREA ESP ', '444', 'Ap', 'CEILANDIA NORTE', 'BRASILIA', '72-999-02', 'DF', NULL, 'Ativo', 3);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
